4e17b52dbb6ea41f77305028a1450d9e74b172d8
Luka <LukaBis@lbiskupic05@gmail.com>
2025-04-15 23:42:34 +0200

    style: pint

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Database\Factories\ClientFactory;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ClientSeeder extends Seeder
